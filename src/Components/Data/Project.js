69219dbd53d9315a288695dfdbbdf4d017f664eb
Nitesh <niteshchandrakar2@gmail.com>
2023-07-11 17:20:16 +0530

    tr4

--- src/Components/Data/Project.js
+++ src/Components/Data/Project.js
@@ -2,7 +2,7 @@
 export const project = [
     {
         id: 0,
-        img: "1.png",
+        img: "https://github.com/niteshchandrakar/niteshchandrakar.github.io/blob/master/src/Images/Screenshot%20(291).png?raw=true",
         title: "HealthMart",
         description: "A website similar to HealthKart, offering an e-commerce platform for health supplements, body-building products, and proteins.",
         tech: ["HTML", "CSS", "JAVASCRIPT"],
@@ -12,17 +12,17 @@ export const project = [
     },
     {
         id: 1,
-        img: "",
+        img: "https://github.com/niteshchandrakar/niteshchandrakar.github.io/blob/master/src/Images/Screenshot%20(292).png?raw=true",
         title: "Edutute",
         description: "A website similar to Student Dashboard that replicates the functionality of Calendly, a popular online scheduling platform",
-        tech: ["HTML", "CSS", "JAVASCRIPT", "CHAKRA_UI", "REACT"],
+        tech: ["HTML", "CSS", "JAVASCRIPT", "CHAKRA-UI", "REACT"],
         links: ["https://github.com/niteshchandrakar/quiet-boundary-1362", "https://649018b178649d408aa5bbc4--effervescent-sfogliatella-b35a0a.netlify.app/"]
 
 
     },
     {
         id: 2,
-        img: "",
+        img: "https://github.com/niteshchandrakar/niteshchandrakar.github.io/blob/master/src/Images/1.png?raw=true",
         title: "Riverhunt",
         description: "A website similar to buy fishing products,in this website you can buy ,making us your ultimate destination for all things fishing.",
         tech: ["HTML", "CSS", "JAVASCRIPT"],
