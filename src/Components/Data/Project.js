d15fb6d223b111f099c8b3bbc13bb10541758822
Nitesh <niteshchandrakar2@gmail.com>
2023-08-02 08:58:16 +0530

    asd

--- src/Components/Data/Project.js
+++ src/Components/Data/Project.js
@@ -5,7 +5,7 @@ export const project = [
         img: "https://github.com/niteshchandrakar/niteshchandrakar.github.io/blob/master/src/Images/Screenshot%20(291).png?raw=true",
         title: "HealthMart",
         description: "A website similar to HealthKart, offering an e-commerce platform for health supplements, body-building products, and proteins.",
-        tech: ["HTML", "CSS", "JAVASCRIPT"],
+        tech: ["HTML", "CSS", "Javascript"],
         links: ["https://github.com/niteshchandrakar/fond-channel-7524", "https://peaceful-blancmange-43db0c.netlify.app/"]
 
 
@@ -15,8 +15,8 @@ export const project = [
         img: "https://github.com/niteshchandrakar/niteshchandrakar.github.io/blob/master/src/Images/Screenshot%20(292).png?raw=true",
         title: "Edutute",
         description: "A website similar to Student Dashboard that replicates the functionality of Calendly, a popular online scheduling platform",
-        tech: ["HTML", "CSS", "JAVASCRIPT", "CHAKRA-UI", "REACT"],
-        links: ["https://github.com/niteshchandrakar/quiet-boundary-1362", "https://649018b178649d408aa5bbc4--effervescent-sfogliatella-b35a0a.netlify.app/"]
+        tech: ["HTML", "CSS", "Javascript", "Chakra-Ui", "React"],
+        links: ["https://github.com/niteshchandrakar/quiet-boundary-1362", "https://teal-sunflower-bcdecf.netlify.app/"]
 
 
     },
@@ -25,7 +25,7 @@ export const project = [
         img: "https://github.com/niteshchandrakar/niteshchandrakar.github.io/blob/master/src/Images/1.png?raw=true",
         title: "Riverhunt",
         description: "A website similar to buy fishing products,in this website you can buy ,making us your ultimate destination for all things fishing.",
-        tech: ["HTML", "CSS", "JAVASCRIPT"],
+        tech: ["HTML", "CSS", "Javascript"],
         links: ["https://github.com/niteshchandrakar/astute-line-8992", "https://stellular-cendol-ad797d.netlify.app/"]
 
 
@@ -33,10 +33,10 @@ export const project = [
     {
         id: 3,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRl8xFytGODn2dSaocMxQRiesw_IIk4i5MUIVh9oQDpQ&s",
-        title: "To be announced",
-        description: "Coming soon",
-        tech: ["HTML", "CSS", "JAVASCRIPT"],
-        links: ["https://github.com/niteshchandrakar/astute-line-8992", "https://stellular-cendol-ad797d.netlify.app/"]
+        title: "MoneyMentor",
+        description: "A website providing loan information and connecting users with loan providers, inspired by paisabazar loan information and loan provider website.",
+        tech: ["HTML", "CSS", "Javascript","React","Redux","Chakra-Ui"],
+        links: ["https://github.com/KrishnaVaishnav98/vogue-pocket-8479", "https://money-mentor.netlify.app/"]
 
 
     },
